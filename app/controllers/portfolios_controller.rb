dfc6605611081dd5e1ba35fadc8212746ce21fd6
andrewlloyd0129 <andrewlloyd0129@gmail.com>
2018-05-07 13:42:50 -0600

    added status to protfolios.  got controller wired up

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,67 @@
 class PortfoliosController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
+  # access all: [:index, :show], user: {except: [:destroy, :new, :create, :update, :edit]}, admin: :all
+
+  def index
+   @portfolios = Portfolio.all
+  end
+
+  def new
+    @portfolio = Portfolio.new
+  end
+
+  def create
+    @portfolio = Portfolio.new(blog_params)
+      if @portfolio.save
+        redirect_to blog_path(@portfolio)
+      else
+        render :new
+      end
+  end
+
+  def edit
+  end
+  
+  def update
+      if @portfolio.update(blog_params)
+        @portfolio.save
+        redirect_to @portfolio, notice: 'Your portfolio was edited successfully'
+      else
+        render :edit
+      end
+  end
+
+  def show
+  end
+  
+  def destroy
+    if @portfolio.destroy
+          redirect_to portfolios_path, notice: 'Your portfolio was destroyed successfully'
+      else
+        render :show, notice: 'Your portfolio could not be destroyed'
+      end
+  end
+
+
+
+  def toggle_status
+    if @portfolio.draft?
+      @portfolio.published!
+    elsif @portfolio.published?
+      @portfolio.draft!
+    end      
+    redirect_to portfolios_path, notice:  "#{@portfolio.title} status has been updated."
+  end
+
+
+  private
+
+    def set_portfolio
+      @portfolio = Portfolio.find(params[:id])
+    end
+
+    def portfolio_params
+          params.require(:portfolio).permit(:title, :description)
+    end
+
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,2 +1,4 @@
 class Portfolio < ApplicationRecord
+    enum status: { draft: 0, published: 1}
+    validates_presence_of :title, :description
 end
