f306e7b65ffade18235dc9d149cae5d91286b3ee
Wnokes <westonroynokes@gmail.com>
2018-05-09 13:11:50 -0600

    Mostly styles

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,3 +37,20 @@ It is a long established fact that a reader will be distracted by the readable c
 
 ", status: 0)
 end
+
+User.create!(
+   email: "admin@test.com",
+   password: "asdfasdf",
+   password_confirmation: "asdfasdf",
+   roles: "admin"
+)
+
+puts "1 admin user created"
+
+User.create!(
+   email: "test@test.com",
+   password: "asdfasdf",
+   password_confirmation: "asdfasdf"
+)
+
+puts "1 regular user created"
