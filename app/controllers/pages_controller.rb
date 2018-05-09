3aab67d752ac626d7a281d906b74f351c486772e
Dawnfable <DuskDawnfable@gmail.com>
2018-05-09 13:16:10 -0600

    fixed all of mailers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,4 @@
 class PagesController < ApplicationController
   def home
   end
-
-  def contact
-        @message = Message.new
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,10 +17,11 @@ Rails.application.routes.draw do
 
   get 'contact', to: 'messages#new', as: 'new_message'
   post 'contact', to: 'messages#create', as: 'create_message'
+  
+  get 'support', to: 'support#new', as: 'new_support'
+  post 'support', to: 'support#create', as: 'create_support'
 
   get 'pages/home'
-
-  get 'pages/contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
