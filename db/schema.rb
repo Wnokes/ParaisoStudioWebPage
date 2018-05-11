222b375c236c98c2a2dba16355e943f27f8176bd
Dawnfable <DuskDawnfable@gmail.com>
2018-05-11 16:25:58 -0600

    final changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20180510203027) do
-
+ActiveRecord::Schema.define(version: 20180511163708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +47,7 @@ ActiveRecord::Schema.define(version: 20180510203027) do
     t.integer "position"
     t.string "slug"
     t.string "url"
-
+    t.string "subtitle"
   end
 
   create_table "users", force: :cascade do |t|
